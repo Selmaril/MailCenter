bac291a85ec7856417f3b4304b23162e999d5a66
Selmaril <selmaril@gmail.com>
2019-03-25 12:25:03 +0300

    begin

---
+++ MailCenter/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MailCenter.Models;
+
+namespace MailCenter.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
